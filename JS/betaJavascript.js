a0ef7483d5ce4d1491a1831ccac2dac28d8a2891
Muhammad Muhaimin <muhammad.r.muhaimin@gmail.com>
2013-10-23 04:47:39 -0400

    Initial Upload as a page

---
+++ JS/betaJavascript.js
@@ -0,0 +1,136 @@
+
+var directionResponse;
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+var markers = new Array();
+
+function initialize() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  var mapOptions = {
+    zoom:7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    }, function() {
+      map.setCenter(chicago);
+    });
+  }
+
+  directionsDisplay.setMap(map);
+  calcRoute();
+}
+
+function calcRoute() {
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+
+  if (start == end) return;
+
+  var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.DirectionsTravelMode.DRIVING
+  };
+
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      directionResponse = response;
+      testDirection(directionResponse);
+    }
+  });
+}
+
+function testDirection(directionResponse2) {
+	var myRoute = directionResponse2.routes[0].legs[0];
+	var spacing = 160 * 1000; //distance is measured in meters
+	var runningTotal = spacing;
+
+	//Export coordinates as array
+	var coordinates = new Array();
+
+	for(var i = 0; i < myRoute.steps.length; i++) {
+		var stepDistance = myRoute.steps[i].distance.value;
+		runningTotal -= stepDistance;
+		while(runningTotal <= 0) { //If less than 0, then in this path, we are passing the spacing mark
+			var distancePerPoint = stepDistance / myRoute.steps[i].path.length;
+			var d = (stepDistance + runningTotal) / distancePerPoint;
+
+			var goodPoint = myRoute.steps[i].path[Math.round(d)];
+
+			coordinates.push(goodPoint); //Add to our coordinates
+
+			runningTotal += spacing;
+		}
+	}
+
+	//Add beginning and ending points
+	coordinates.push(myRoute.start_location);
+	coordinates.push(myRoute.end_location);
+
+	clearMarkers();
+
+	for(var i = 0; i < coordinates.length; i++) {
+		wunderGroundFetch(coordinates[i], map);
+	}
+	return coordinates;
+}
+
+function wunderGroundFetch(coordinate, map){
+	var key = "31e5badd4441213c";
+	var url = "http://api.wunderground.com/api/"+key+"/conditions/q/"+coordinate.lat()+","+coordinate.lng()+".json";
+	var weather; 
+	var temp;
+	var humidity; 
+				
+	jQuery(document).ready(function($) { 
+		weather = $.ajax({ 
+			url : url, 
+			dataType : "jsonp", 
+			success : function(parsed_json) { 
+				weather = parsed_json['current_observation']['icon_url']; 
+				temp = parsed_json['current_observation']['temperature_string'];
+				humidity = parsed_json['current_observation']['relative_humidity']; 
+
+				setMarker(coordinate, map, weather, temp);
+			}
+			
+		});
+	});
+}
+
+function clearMarkers() {
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(null);
+		}
+	markers = [];
+}
+
+function setMarker(coordinate, map, image, temp) {
+	var marker = new google.maps.Marker({
+	        position: coordinate,
+	        icon: image,
+	        map: map
+   	});
+
+	var markerLabel = new MapLabel({
+		position: coordinate,
+		text: temp,
+		map: map, 
+		strokeWeight: 10
+	})
+
+   	markers.push(marker);
+   	markers.push(markerLabel);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
---
+++ README.md
@@ -0,0 +1,10 @@
+Travelive
+=========
+
+Hack MIT Project
+
+TODO:
+=======
+- Add attractions 
+- Make suggestions for attractions based on weather and time distance
+- Add pictures for Attractions (Locu?)
